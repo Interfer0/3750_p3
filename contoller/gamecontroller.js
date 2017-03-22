5c67313231647aec425d7add10701a7a3584ab74
Quinn Anderson <richqanderson@gmail.com>
2017-03-22 17:55:12 -0600

    updating gamecontroller with socket io

--- contoller/gamecontroller.js
+++ contoller/gamecontroller.js
@@ -1,20 +1,59 @@
+module.exports = (io) => {
+
+    // server side tracker of logged in users
+    // used to populate initial list of online users
+    let users = [];
+    var running_games = [];
+        /*
+        ie:  running_game({roomid:1234,name:1234});
+        ***user can change the name property
+        */
+    io.sockets.on('connection', socket => {
+        
+        // TODO: show all current usernames in users div on chatroom page
+        
+        const user = { name: socket.request.user.name, username: socket.request.user.username };
+
+        running_games.push({roomid:1234,name:newroom});
+
+        function namechange(roomname,room){
+            var hostname = running_games.filter(function(item){
+                return item.name == roomname;
+            });
+        }
+
+        // do the the following on connection 
+        socket.broadcast.emit('userLoggedIn', user.username);
+        users.push(user.username);
+        
+        socket.on('getUser', (callback) => {
+            io.sockets.emit('whoami', user);
+        });
+
+        socket.on('getUsers', (callback) => {
+            io.to(socket.id).emit('userList', users); // only send userList to newly connected user
+        });
+        
+        // Client to Server message
+        socket.on('c2smsg', function(data, callback){
+            var chatObject = {person: user.username, message: data};
+            socket.broadcast.emit('s2cmsg', chatObject);
+        });
+
+        // Notify the chat room the user disconnected
+        // update the server side tracker
+        socket.on('disconnect', socket => {
+            io.sockets.emit('userLoggedOut', user.username);
+            users.splice(users.indexOf(user.username),1); // remove from user tracker
+        });
+
+    });// end on connection event
+
+};
 
 
-var running_games = [];
-/*
-ie:  running_game({roomid:1234,name:1234});
-***user can change the name property
-*/
 
-running_games.push({roomid:1234,name:newroom});
 
-function namechange(roomname,room){
-   if(running_games.indexOf(roomname) >= 0){
-       return "room already being used";
-   }else{
-       running_games.push({roomid:room, name:roomname});
-   }
-}
 
 users = [];
 
