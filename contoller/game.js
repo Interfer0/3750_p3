45dfa906e566d071e40ce614c54232aa14adcf14
Quinn Anderson <richqanderson@gmail.com>
2017-03-22 12:32:08 -0600

    waitForPlayers view

--- contoller/game.js
+++ contoller/game.js
@@ -3,22 +3,24 @@ var gamecontoller = require("./gamecontroller")
 var gameRounds;
 function createGame(req, res){
     console.log("Game creation has been initialized");
-    gameRounds = req.round;
+    gameRounds = req.rounds;
 }
 
 users = [];
-function addUserToRoom(socket, {user:username, user:roomid}){
+function addUserToRoom(response, socket, {user:username, user:roomid}){
     socket.join(user.roomid);
     //socket.to(user.roomid).emit(function_name);
     socket.to(user.roomid).emit(user.username + " has join the game.");
     users.push({user:roomid, user:username});
     console.log(user.username + " has joined " + user.roomid);
+    response.render('user',{username:user.username});
 }
 
 function randomHost(response){
     var x = users.length;
     if(x > 1 && gameRounds > 0){
         var host = users[Math.floor(Math.random() * x)];
+        return host;
     }else{
         return "wait for more players";
     }
