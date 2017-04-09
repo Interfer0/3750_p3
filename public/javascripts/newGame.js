7ff425327343c18d0b1e09889c9e0e23663ec360
Stewart0822 <A.Stewart0822@gmail.com>
2017-04-09 17:17:59 -0600

    Javascript (game.js) validation for text fields on initialGame.pug, and newGame.pug

--- public/javascripts/newGame.js
+++ public/javascripts/newGame.js
@@ -1,12 +1,11 @@
-var submit = document.getElementById("newGameSubmitButton");
-var roomName = document.getElementById("roomInput");
+function init() {
+    function textFieldValidate(event) {
+       
+        if(roomName.value === ""){
+        roomName.style = "background-color:#99ff99";
+        }
+    }
 
-$(document).on("click", "newGameSubmitButton", textFieldValidate)
-
-function textFieldValidate(){
-        if(roomName.innerHTML == "")
-            roomName.style.background = "#555555";
-
-
-
+$('#newGameSubmitButton').on('click', textFieldValidate);
 }
+$(document).on('ready', init);      
